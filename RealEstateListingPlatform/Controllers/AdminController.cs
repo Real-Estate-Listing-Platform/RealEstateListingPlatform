468d096e804dd88cb3b01c703cb7c3e88b93b855
Trần Phương Huyền <tranphuonghuyen2005@gmail.com>
2026-01-21 09:09:13 +0700

    Add admin listing operation page

---
+++ RealEstateListingPlatform/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateListingPlatform.Controllers
+{
+    public class AdminController : Controller
+    {
+        // Trang Dashboard tổng quan
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Trang danh sách bất động sản
+        public IActionResult Listings()
+        {
+            return View();
+        }
+    }
+}
